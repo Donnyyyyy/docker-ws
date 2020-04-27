a3b31bc812783374b32f478602a8442b36b36e3f
Dmitrii Donetskii <don.skoy.333@gmail.com>
2020-04-27 16:30:54 +0300

    no-docker setup

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from redis import Redis
+from rq import Queue
+from rq.job import Job
+from rq.exceptions import NoSuchJobError
+
+from task import process_user
+
+redis = Redis()
+q = Queue(connection=redis)
+app = Flask(__name__)
+
+@app.route('/<uid>')
+def enqueue(uid):
+    job = q.enqueue(process_user, uid)
+    return job.id
+
+@app.route('/result/<job_id>')
+def get_job_result(job_id):
+    try:
+        job = Job.fetch(job_id, connection=redis)
+    except NoSuchJobError:
+        return 'No such job'
+
+    return job.result if job.result else 'Not yet completed'
+
+app.run(host='0.0.0.0', port=8080)
---
+++ task.py
@@ -0,0 +1,2 @@
+def process_user(uid):
+    return uid * 10
